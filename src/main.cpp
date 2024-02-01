b621912c8b5965d217496cf605ff028e7e745539
Akshay Pal <akshay.anu@gmail.com>
2024-02-01 21:14:54 +0000

    created inital window

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include "raylib.h"
+#include <iostream>
+#include <string>
 
-int main(void)
-{
-    InitWindow(800, 450, "raylib [core] example - basic window");
+using namespace std;
 
-    while (!WindowShouldClose())
-    {
+const int screenWidth = 1280;
+const int screenHeight = 800;
+
+int main() {
+    std::cout << "Starting the game" << std::endl;
+    InitWindow(screenWidth, screenHeight, "Pong");
+
+    while(!WindowShouldClose()) {
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawText("Hello World!", 0.5*screenWidth, 0.5*screenHeight, 50, RAYWHITE);
         EndDrawing();
     }
 
     CloseWindow();
-
     return 0;
 }
