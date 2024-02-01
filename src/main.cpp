b1af798a766c8a52522b7e43bce8a5edac7bfa2e
Akshay Pal <akshay.anu@gmail.com>
2024-02-01 21:34:59 +0000

    draw ball, paddle and halfway line

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,25 @@ using namespace std;
 
 const int screenWidth = 1280;
 const int screenHeight = 800;
+const int paddleWidth = 25;
+const int paddleHeight = 120;
+const int paddlePadding = 25;
 
-int main() {
+
+int main() 
+{
+    
     std::cout << "Starting the game" << std::endl;
     InitWindow(screenWidth, screenHeight, "Pong");
+    SetTargetFPS(60);
 
-    while(!WindowShouldClose()) {
+    while(!WindowShouldClose()) 
+    {
         BeginDrawing();
-            DrawText("Hello World!", 0.5*screenWidth, 0.5*screenHeight, 50, RAYWHITE);
+            DrawCircle(0.5*screenWidth, 0.5*screenHeight, 10, WHITE);
+            DrawRectangle(paddlePadding, 0.5*screenHeight - 0.5*paddleHeight, paddleWidth, paddleHeight, WHITE);
+            DrawRectangle(screenWidth - paddleWidth - paddlePadding, 0.5*screenHeight - 0.5*paddleHeight, paddleWidth, paddleHeight, WHITE);
+            DrawLine(0.5*screenWidth, screenHeight, 0.5*screenWidth, 0, WHITE);
         EndDrawing();
     }
 
